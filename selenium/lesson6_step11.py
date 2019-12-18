1302980624d11a1b51f3b851cdfd2cea572c0c15
Mikhail Sukhorukov <Mikhail_Sukhorukov@epam.com>
2019-12-18 16:17:06 +0300

    added virtualenv, pyton files with lessons

--- selenium/lesson6_step11.py
+++ selenium/lesson6_step11.py
@@ -1,11 +1,9 @@
 from selenium import webdriver
-from selenium.webdriver.support.ui import Select
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
 import math
-import os
 
 #link = "http://suninjuly.github.io/wait1.html"
 try:
---
+++ selenium/test_unit.py
@@ -0,0 +1,62 @@
+import unittest
+import time
+from selenium import webdriver
+
+
+class NewTests(unittest.TestCase):
+
+    def test_first(self):
+        link = "http://suninjuly.github.io/registration1.html"
+        browser = webdriver.Chrome()
+        browser.get(link)
+
+        # Ваш код, который заполняет обязательные поля
+        first_name = browser.find_element_by_css_selector('[placeholder="Input your first name"]').send_keys("d")
+        second_name = browser.find_element_by_css_selector('[placeholder="Input your last name"]').send_keys("d")
+        email = browser.find_element_by_css_selector('[placeholder="Input your email"]').send_keys("d")
+
+        # Отправляем заполненную форму
+        button = browser.find_element_by_css_selector("button.btn")
+        button.click()
+
+        # Проверяем, что смогли зарегистрироваться
+        # ждем загрузки страницы
+        time.sleep(1)
+
+        # находим элемент, содержащий текст
+        welcome_text_elt = browser.find_element_by_tag_name("h1")
+        # записываем в переменную welcome_text текст из элемента welcome_text_elt
+        welcome_text = welcome_text_elt.text
+
+        # с помощью assert проверяем, что ожидаемый текст совпадает с текстом на странице сайта
+        self.assertEqual("Congratulations! You have successfully registered!", welcome_text)
+
+    def test_second(self):
+        link = "http://suninjuly.github.io/registration2.html"
+        browser = webdriver.Chrome()
+        browser.get(link)
+
+        # Ваш код, который заполняет обязательные поля
+        first_name = browser.find_element_by_css_selector('[placeholder="Input your first name"]').send_keys("d")
+        second_name = browser.find_element_by_css_selector('[placeholder="Input your last name"]').send_keys("d")
+        email = browser.find_element_by_css_selector('[placeholder="Input your email"]').send_keys("d")
+
+        # Отправляем заполненную форму
+        button = browser.find_element_by_css_selector("button.btn")
+        button.click()
+
+        # Проверяем, что смогли зарегистрироваться
+        # ждем загрузки страницы
+        time.sleep(1)
+
+        # находим элемент, содержащий текст
+        welcome_text_elt = browser.find_element_by_tag_name("h1")
+        # записываем в переменную welcome_text текст из элемента welcome_text_elt
+        welcome_text = welcome_text_elt.text
+
+        # с помощью assert проверяем, что ожидаемый текст совпадает с текстом на странице сайта
+        self.assertEqual("Congratulations! You have successfully registered!", welcome_text)
+
+
+if __name__ == "__main__":
+    unittest.main()
